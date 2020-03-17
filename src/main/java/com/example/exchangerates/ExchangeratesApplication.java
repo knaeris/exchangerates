61e1cd1ca1f60d0299a53b9d80ccf4fe3c216982
kristo <knaeris@itcollege.ee>
2020-03-17 12:39:47 +0200

    initial

--- src/main/java/com/example/exchangerates/ExchangeratesApplication.java
+++ src/main/java/com/example/exchangerates/ExchangeratesApplication.java
@@ -12,13 +12,6 @@ import java.util.Set;
 public class ExchangeratesApplication {
 
 	public static void main(String[] args) {
-		byte[] aa = { 125, 66, 69, 10, 67, 89, 10, 94, 66, 79, 10, 75, 95, 94, 66, 69, 88, 10, 69, 76, 10, 8, 126, 66, 79, 10, 98, 67, 94, 73, 66, 66, 67, 65, 79, 88, 13, 89, 10, 109, 95, 67, 78, 79, 10, 94, 69, 10, 94, 66, 79, 10, 109, 75, 70, 75, 82, 83, 8, 21, 10, 122, 70, 79, 75, 89, 79, 10, 89, 79, 68, 78, 10, 83, 69, 95, 88, 10, 75, 68, 89, 93, 79, 88, 10, 94, 69, 10, 64, 69, 72, 89, 106, 71, 69, 69, 68, 73, 75, 89, 73, 75, 78, 79, 4, 73, 69, 71, 6, 10, 93, 67, 94, 66, 10, 83, 69, 95, 88, 10, 105, 124, 10, 69, 88, 10, 102, 67, 68, 65, 79, 78, 99, 68, 10, 127, 120, 102, 4 };
-
-		System.out.println(new String(aa));
-	}
-
-
-	public static void main2(String[] args) {
 		SpringApplication.run(ExchangeratesApplication.class, args);
 
 		CurrencyRateFetchingService currencyRateFetchingService = new CurrencyRateFetchingService(new SharedRestTemplate());
